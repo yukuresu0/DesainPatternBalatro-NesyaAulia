d5565af4653864cbc5479e6412c528cdaf94c6ac
vctrnl <victurnalphantom@gmail.com>
2026-05-06 12:17:15 +0700

    Game Manager
    Game manager h cpp

---
+++ GameManager.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "GameManager.h"
+
+void GameManager::runSession() {
+    std::cout << "=== Run Started ===\n";
+
+    Hand hand = handGenerator.generateHand();
+    handPlayer.playHand(hand);
+
+    int score = scoringRule.scoreHand(hand);
+    bool win = blindRule.checkBlind(score);
+    int reward = rewardRule.earnMoney(win, score);
+
+    std::cout << "Money gained: " << reward << "\n";
+    std::cout << "=== Run Ended ===\n";
+}
